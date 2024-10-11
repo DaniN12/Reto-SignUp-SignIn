318f8ebbefd5e973ded61629c6d3f5fe5ac81377
2dam <2dam@TartangaLH.eus>
2024-10-11 08:32:08 +0200

    sign up window finished

--- ClientApp/src/clientapp/SignInController.java
+++ ClientApp/src/clientapp/SignInController.java
@@ -7,18 +7,26 @@ package clientapp;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
- * FXML Controller class
  *
  * @author 2dam
  */
-public class SingUpViewController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+public class SignInController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
