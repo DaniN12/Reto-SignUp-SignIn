1f8479da3212db879044ca7181c010350d3a4cef
2dam <2dam@TartangaLH.eus>
2024-10-16 09:24:38 +0200

    Todo todito

--- ClientApp/src/clientapp/controller/SignInController.java
+++ ClientApp/src/clientapp/controller/SignInController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package clientapp;
+package clientapp.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
