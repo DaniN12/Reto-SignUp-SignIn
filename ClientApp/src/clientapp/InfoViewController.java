b2501cf0ad9f4b2a32140dede30bc6672db89621
2dam <2dam@TartangaLH.eus>
2024-10-17 08:56:20 +0200

    Controlador ventana view necesito factoria

--- ClientApp/src/clientapp/InfoViewController.java
+++ ClientApp/src/clientapp/InfoViewController.java
@@ -7,7 +7,10 @@ package clientapp;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -17,11 +20,54 @@ import javafx.fxml.Initializable;
 public class InfoViewController implements Initializable {
 
     /**
+     * Button used to log out user sesion
+     */
+    
+    @FXML
+    private Button logOutBtn;
+
+    /**
+     * Field where the email will be displayed
+     */
+    
+    @FXML
+    private TextField emailTextF;
+
+    /**
+     *Field where the street will be displayed
+     */
+    
+    @FXML
+    private TextField streetTextF;
+
+    /**
+     *Field where the name will be displayed
+     */
+    
+    @FXML
+    private TextField userrNameTextF;
+
+    /**
+     *Field where the city will be displayed
+     */
+    
+    @FXML
+    private TextField citylTextF;
+
+    /**
+     *Field where the zip will be displayed
+     */
+    
+    @FXML
+    private TextField zipTextF;
+
+    /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        
+    }
+
 }
